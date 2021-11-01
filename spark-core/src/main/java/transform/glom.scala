997dbdbfb4acae2b925d25a156d1e112dddd43db
zhouhengtao <java_ht@163.com>
2021-11-01 14:21:35 +0800

    这一步是glom的使用

---
+++ spark-core/src/main/java/transform/glom.scala
@@ -0,0 +1,23 @@
+package transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object glom {
+  def main(args: Array[String]): Unit = {
+
+    //将同一个分区的数据转换为相同类型的内存数组进行处理
+    val  sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("test")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd: RDD[Int] = sc.makeRDD(List(1,2,3,4,5,6), 2)
+    val glomRdd: RDD[Array[Int]] = rdd.glom()
+
+   glomRdd.collect().foreach(
+   data =>println(data.mkString(","))
+   )
+
+
+    sc.stop()
+  }
+}
