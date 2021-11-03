ad64ddb82cf8ef5b5db32a2ca519e47682e81df8
周靓仔 <java_ht@163.com>
2021-11-03 23:27:23 +0800

    明天总结  一定要总结

--- spark-core/src/main/java/transform_2Value/aggregateByKey.scala
+++ spark-core/src/main/java/transform_2Value/aggregateByKey.scala
@@ -10,18 +10,26 @@ object aggregateByKey {
     val sc = new SparkContext(SparkConf)
 
 
-    //将数据根据不同的规则进行分区内计算和分区间计算
-    //第一个参数是
-    val rdd: RDD[(String, Int)] = sc.makeRDD(List(("a", 1), ("a", 2), ("c", 3),("d",4)),4)
+    //将数据根据不同的规则进行分区内计算和分区间计算。注意和reduceBykey(分区内和分区间的规则一样)比较
+    //
+    val rdd: RDD[(String, Int)] = sc.makeRDD(List(("a", 1), ("a", 2), ("a", 3),("a",4)),2)
 
-    val value: RDD[(String, Int)] = rdd.aggregateByKey(0)(_ + _, _ + _)
+        //aggregateByKey存在函数柯里化 有两个参数列表
+    // 第二个参数列表中第一个参数是分区内计算规则  第二个参数是分区间计算规则      math.min(x,y) 两者取最小
+    //第一个()中的值 主要是为了和第一个数作比较
+    val aggRdd: RDD[(String, Int)] = rdd.aggregateByKey(0)(
+      (x, y) => math.max(x, y),
+      (x, y) => x + y
+    )
+
+   // val value: RDD[(String, Int)] = rdd.aggregateByKey(0)(_ + _, _+_ )
 
 
 
-    println("这个是分区：  "+value.partitions.size)
+    println("这个是分区：  "+aggRdd.partitions.size)
 
 
-    value.collect().foreach(println)
+    aggRdd.collect().foreach(println)
 
     sc.stop()
   }
