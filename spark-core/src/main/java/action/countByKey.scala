58c22ef91f0159b5dbe11585bb403a8c32eb9950
周靓仔 <java_ht@163.com>
2021-11-06 19:36:47 +0800

    action算子

---
+++ spark-core/src/main/java/action/countByKey.scala
@@ -0,0 +1,38 @@
+package action
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object countByKey {
+  def main(args: Array[String]): Unit = {
+
+    val SparkConf = new SparkConf().setMaster("local[*]").setAppName("MyTest")
+    val sc = new SparkContext(SparkConf)
+
+
+
+/*    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4))
+    val value: collection.Map[Int, Long] = rdd.countByValue()
+
+    println(value) //表示值出现的次数
+    */
+
+
+    //统计每种 key 的个数
+ val rdd: RDD[(Int, String)] = sc.makeRDD(List((1, "a"), (1, "a"), (1, "b"), (2, "b"), (3, "d"), (3, "c")))
+
+
+    val countByVal: collection.Map[Int, Long] = rdd.countByKey()
+
+    println(countByVal)
+
+    // 结果 Map(1 -> 3, 2 -> 1, 3 -> 2)
+
+
+
+    sc.stop()
+
+
+  }
+
+}
---
+++ spark-core/src/main/java/action/reduce.scala
@@ -0,0 +1,27 @@
+package action
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object reduce {
+  def main(args: Array[String]): Unit = {
+
+    val SparkConf = new SparkConf().setMaster("local[*]").setAppName("MyTest")
+    val sc = new SparkContext(SparkConf)
+
+    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4),4)
+
+     //行动算子会触发作业(job)的执行的方法
+    //底层调用的是环境对象的runjob方法
+    //底层代码会创建ActiveJob  并提交执行
+
+    println(rdd.reduce(_ + _))//两两聚合
+
+
+
+    sc.stop()
+
+
+  }
+
+}
