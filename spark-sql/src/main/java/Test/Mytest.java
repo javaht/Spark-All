875a435a2b7e1dd29f721139878f1bba98fb870e
周靓仔 <java_ht@163.com>
2021-11-16 23:22:09 +0800

    练手项目

--- spark-sql/src/main/java/Test/Mytest.java
+++ spark-sql/src/main/java/Test/Mytest.java
@@ -1,23 +1,25 @@
 package Test;
 
+
+
+
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
-
 
 import java.util.List;
 
 public class Mytest {
     public static void main(String[] args) {
 
+
         SparkConf sparkConf = new SparkConf().setAppName("我的缓存表测试").setMaster("local[*]");
         SparkSession spark = SparkSession.builder().config(sparkConf).getOrCreate();
 
 
 
-        List<Alldate> allDateList = spark.read().format("jdbc").option("url", "jdbc:mysql://127.0.0.1:3306/testByzht?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Hongkong")
-                .option("dbtable", "all_date").option("user", "root").option("password", "123456").load()
+        List<Alldate> allDateList = spark.read().format("jdbc").option("url", "jdbc:mysql://192.168.20.62:3306/gmall?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Hongkong")
+                .option("dbtable", "base_dic").option("user", "root").option("password", "123456").load()
                 .as(Encoders.bean(Alldate.class)).collectAsList();
 
         for (int i = 0; i < allDateList.size(); i++) {
@@ -27,6 +29,7 @@ public class Mytest {
 
 
         spark.stop();
+
 
 
     }
