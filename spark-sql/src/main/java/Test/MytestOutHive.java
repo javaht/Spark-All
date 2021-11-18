ef947c715b41df906941a78ec06d3bc5e55fcdaa
周靓仔 <java_ht@163.com>
2021-11-18 21:32:29 +0800

    spark操作hive和mysql

--- spark-sql/src/main/java/Test/MytestOutHive.java
+++ spark-sql/src/main/java/Test/MytestOutHive.java
@@ -3,7 +3,9 @@ package Test;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 
 public class MytestOutHive {
     public static void main(String[] args) {
@@ -11,10 +13,15 @@ public class MytestOutHive {
 
         SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("MyHiveTest");
         SparkSession spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate();
+        Dataset<Row> dataset = spark.table("gmall.dim_base_province").where("province_name is not null ");
 
-        spark.sql("show tables").show();
 
 
+
+         dataset.select(dataset.col("province_name").cast(DataTypes.StringType)).write().format("jdbc")
+                 .option("url", "jdbc:mysql://192.168.20.62:3306/gmall?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Hongkong")
+                 .option("dbtable","provice").option("user", "root").option("password", "123456").mode(SaveMode.Overwrite)
+                 .save();
 
         spark.stop();
     }
