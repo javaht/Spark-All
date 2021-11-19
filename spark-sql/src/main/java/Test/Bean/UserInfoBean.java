613c3f2fdec4d6db0f424f4a8e4a10497db2170d
zhouhengtao <java_ht@163.com>
2021-11-19 16:18:27 +0800

    读取数据并且做相应的转换

---
+++ spark-sql/src/main/java/Test/Bean/UserInfoBean.java
@@ -0,0 +1,61 @@
+package Test.Bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoBean {
+    private Long aac001;
+    private String aac003;
+    private String aac147;
+    private String aac004;
+    private String aac006;
+    private String aae005;
+    private String aae321;
+    private String ala040;
+    private String alc060;
+    private String cac220;
+    private String cac220_name;
+    private String aac005;
+    private String aac005_name;
+    private String aac012;
+    private String cac221;
+    private String aac020;
+    private String aac114;
+    private String aac007;
+    private String aac184;
+    private String aac185;
+    private String cac217;
+    private String cac218;
+    private String cac219;
+    private String cac205;
+    private String aaf013;
+    private String aac010;
+    private String aae007;
+    private String aae138;
+    private String aac060;
+    private String aac060_name;
+    private String aac011;
+    private String aac011_name;
+    private String aac024;
+    private String aac024_name;
+    private String aac009;
+    private String aac009_name;
+    private String aac017;
+    private String aac017_name;
+    private String aac183;
+    private String aac183_name;
+    private String aac028;
+    private String aac028_name;
+    private String aac033;
+    private String aac033_name;
+    private String aac084;
+    private String aac084_name;
+    private String aac008;
+    private String aac008_name;
+    private String aac014;
+    private String aac014_name;
+    private String aac015;
+    private String aac015_name;
+}
--- spark-sql/src/main/java/Test/Spark_Mysql_test.java
+++ spark-sql/src/main/java/Test/Spark_Mysql_test.java
@@ -1,10 +1,13 @@
 package Test;
 
+import Test.Bean.AC01Bean;
+import Test.Bean.UserInfoBean;
 import org.apache.spark.SparkConf;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class Spark_Mysql_test {
     public static void main(String[] args) {
@@ -17,22 +20,80 @@ public class Spark_Mysql_test {
         SparkConf sparkConf = new SparkConf().set("spark.sql.warehouse.dir", "hdfs://192.168.2.240:8020/user/hive/warehouse");
         SparkSession spark = SparkSession.builder().config(sparkConf).appName("sparkMysqlTest").master("local[*]").enableHiveSupport().getOrCreate();
 
+/*
 
         Dataset<Row> rowDataset = spark.read().format("jdbc").option("url", "jdbc:mysql://127.0.0.1:3306/testByzht?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Hongkong")
                 .option("user", "root").option("password", "123456").option("dbtable", "all_date").load();
+*/
+        Dataset<Row> aa10 = spark.table("ods.ods_aa10");
+
+        final HashMap<String, String> codeMap = new HashMap<String, String>();
+        List<Row> aa10List = aa10.collectAsList();
 
 
+        //aa10List.forEach(row -> codeMap.put(row.getString(3)+"_"+row.getString(4),row.getString(5)));
 
-         //这里一般不同saveAsTable   这就百度查查什么原因
+        for (int i = 0; i < aa10List.size(); i++) {
+            codeMap.put(aa10List.get(i).getString(3)+"_"+aa10List.get(i).getString(4),      aa10List.get(i).getString(5));
+        }
+        //(AAC183_484,三级飞行通信员)
+
+        Dataset<AC01Bean> ac01 = spark.table("ods.ods_ac01").as(Encoders.bean(AC01Bean.class));
+
+
+        Dataset<UserInfoBean> result = ac01.map(new MapFunction<AC01Bean, UserInfoBean>() {
+            @Override
+            public UserInfoBean call(AC01Bean row) throws Exception {
+                UserInfoBean userInfoBean = new UserInfoBean();
+
+                userInfoBean.setAac005_name(getKey("AAC005", row.getAac005(), codeMap));
+                userInfoBean.setAac008_name(getKey("AAC008", row.getAac008(), codeMap));
+                userInfoBean.setAac009_name(getKey("AAC009", row.getAac009(), codeMap));
+                userInfoBean.setAac011_name(getKey("AAC011", row.getAac011(), codeMap));
+                userInfoBean.setAac060_name(getKey("AAC060", row.getAac060(), codeMap));
+                userInfoBean.setAac011_name(getKey("AAC011", row.getAac011(), codeMap));
+                userInfoBean.setAac024_name(getKey("AAC024", row.getAac024(), codeMap));
+                userInfoBean.setAac017_name(getKey("AAC017", row.getAac017(), codeMap));
+                userInfoBean.setAac183_name(getKey("AAC183", row.getAac183(), codeMap));
+                userInfoBean.setAac028_name(getKey("AAC028", row.getAac028(), codeMap));
+                userInfoBean.setAac033_name(getKey("AAC033", row.getAac033(), codeMap));
+                userInfoBean.setAac084_name(getKey("AAC084", row.getAac084(), codeMap));
+                userInfoBean.setAac014_name(getKey("AAC014", row.getAac014(), codeMap));
+                userInfoBean.setAac015_name(getKey("AAC015", row.getAac015(), codeMap));
+                userInfoBean.setCac220_name(getKey("CAC220", row.getCac220(), codeMap));
+
+                return userInfoBean;
+            }
+        }, Encoders.bean(UserInfoBean.class));
+
+
+        result.where("aac028_name ='农民工' ").show();
+
+        //这里一般不同saveAsTable   这就百度查查什么原因
       /*  rowDataset.select("xm").write().mode(SaveMode.Overwrite).insertInto("default.mytestxm");
         spark.conf().set("spark.sql.legacy.allowCreatingManagedTableUsingNonemptyLocation","true");
 
+
+      rowDataset.select("xm").write().format("hive").mode(SaveMode.Overwrite).saveAsTable("default.mytestxm");
       */
 
 
-        rowDataset.select("xm").write().format("hive").mode(SaveMode.Overwrite).saveAsTable("default.mytestxm");
+
 
         spark.stop();
 
+
+
     }
+
+
+    //key AAA183
+    //code
+    // codeMap = (AAC183_484,三级飞行通信员)
+    private static String getKey(String key,String code, HashMap<String, String> codeMap){
+
+         //codeMap = (AAC183_484,三级飞行通信员) 所以传入key返回value
+          return codeMap.get(key+"_"+code);
+    }
+
  }
