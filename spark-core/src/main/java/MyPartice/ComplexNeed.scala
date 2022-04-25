9334a3f641bc8f27a9d6131aba279a34a9a7ab75
zhouhengtao <java_ht@163.com>
2022-04-25 17:30:38 +0800

    算子练习

--- spark-core/src/main/java/MyPartice/ComplexNeed.scala
+++ spark-core/src/main/java/MyPartice/ComplexNeed.scala
@@ -7,12 +7,40 @@ object ComplexNeed {
 
   def main(args: Array[String]): Unit = {
 
-    //数据汇总为学生ID，姓名，大数据成绩，数学成绩，总分，平均分。
+
 
     val conf: SparkConf = new SparkConf().setAppName("TwoScoreAvg").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    val value: RDD[String] = sc.textFile("xuehao.txt")
+    val xuehaoRdd: RDD[String] = sc.textFile("datas/xuehao.txt")
+    val scoreRdd: RDD[String] = sc.textFile("datas/xueHaoWithScore.txt")
+
+    val xuehaoMapRdd: RDD[(String, String)] = xuehaoRdd.map(
+      line => {
+        val datas: Array[String] = line.split(",")
+        (datas(0), datas(1))
+      }
+    )
+
+    val scoreMapRdd: RDD[(String, (String, String))] = scoreRdd.map(
+      line => {
+        val datas: Array[String] = line.split(",")
+        (datas(0), (datas(1), datas(2)))
+      }
+    )
+    //数据汇总为学生ID，姓名，大数据成绩，数学成绩，总分，平均分。
+    val joinRdd: RDD[(String, Iterable[(String, String, String)])] = xuehaoMapRdd.join(scoreMapRdd).map(
+      line => {
+        val xuhao: String = line._1
+        val name: String = line._2._1
+        val bigData: String = line._2._2._1
+        val math: String = line._2._2._2
+        (xuhao, (name, bigData, math))
+      }
+    ).groupByKey(1)
+
+   // joinRdd.map().foreach(println)
+
 
 
 
