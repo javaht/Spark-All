071769e647b5935331dd71d4c303206d8f9a4823
Administrator <java_ht@163.com>
2022-12-27 08:57:12 +0800

    fix

--- spark-tuning/src/main/java/partition/PartitionDemo.scala
+++ spark-tuning/src/main/java/partition/PartitionDemo.scala
@@ -14,19 +14,15 @@ object PartitionDemo {
 
     //查询出三张表 并进行join 插入到最终表中
     val saleCourse = sparkSession.sql("select * from sparktuning.sale_course")
-    val coursePay = sparkSession.sql("select * from sparktuning.course_pay")
-      .withColumnRenamed("discount", "pay_discount")
-      .withColumnRenamed("createtime", "pay_createtime")
-    val courseShoppingCart = sparkSession.sql("select * from sparktuning.course_shopping_cart")
-      .drop("coursename")
-      .withColumnRenamed("discount", "cart_discount")
-      .withColumnRenamed("createtime", "cart_createtime")
+    val coursePay = sparkSession.sql("select * from sparktuning.course_pay").withColumnRenamed("discount", "pay_discount").withColumnRenamed("createtime", "pay_createtime")
+
+    val courseShoppingCart = sparkSession.sql("select * from sparktuning.course_shopping_cart").drop("coursename").withColumnRenamed("discount", "cart_discount").withColumnRenamed("createtime", "cart_createtime")
 
     saleCourse
       .join(courseShoppingCart, Seq("courseid", "dt", "dn"), "right")
       .join(coursePay, Seq("orderid", "dt", "dn"), "left")
     //  .coalesce(6)
-//      .repartition(6)
+   //   .repartition(6)
       .select("courseid", "coursename", "status", "pointlistid", "majorid", "chapterid", "chaptername", "edusubjectid"
         , "edusubjectname", "teacherid", "teachername", "coursemanager", "money", "orderid", "cart_discount", "sellmoney",
         "cart_createtime", "pay_discount", "paymoney", "pay_createtime", "dt", "dn")
